dbbde72189180becfa14f04b1bd25306dd52f375
ZYROUGE <me@zyrouge.gq>
2021-02-07 22:04:04 +0530

    Update index.js

--- manager/index.js
+++ manager/index.js
@@ -1,11 +1,13 @@
 const cp = require("child_process");
-const until = require("until");
+const until = require("util");
 const fs = require("fs-extra").promises;
 const exec = util.promisify(cp.exec);
 const chalk = require("chalk");
 
 const config = require("../config");
-const output = path.join(__dirname, "..", "docs");
+const base = path.resolve(__dirname, "..");
+const output = path.join(base, "docs");
+const ignored = [];
 
 const Logger = {
 log (text) {
@@ -18,7 +20,11 @@ for (project of config) {
 // const project = await fs.readdir(conf.dir);
 await exec(`cd ${project.dir} && ${project.cmd}`);
 await fs.copy(project.dist, path.join(output, project.name));
+ignored.push(path.join(base, project.name));
 Logger.log(`Compiled ${chalk.greenBright(project.name)}`);
+}
+for (ign of ignored) {
+await fs.remove(ign);
 }
 }
 
