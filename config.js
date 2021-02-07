7450ed300a4b26db7d6d50e029c95b9f7c7f1f92
ZYROUGE <me@zyrouge.gq>
2021-02-07 21:06:35 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+const path = require("path");
+
+const createProject = ({ dir, cmd, build }) => {
+const src = path.join(__dirname, dir);
+const dist = path.join(src, build);
+return { src, cmd, dist };
+}
+
+module.exports = [
+createProject({ dir: "shopping-app", build: "dist", cmd: "npm run dev" }),
+];
